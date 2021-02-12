9dd6eaef41cf53d78e080933d82ea11d87a784b8
Zhenya <e.mezhueva@gmail.com>
2021-02-12 13:32:13 +0200

    solution

--- src/helpers.js
+++ src/helpers.js
@@ -1,4 +1,4 @@
 
-export const getPhones = () => fetch('/api/phones.json')
+export const getPhones = () => fetch('https://zhenya-mezhueva.github.io/phone-catalogue/api/phones.json')
     .then(response => response.json());
 
